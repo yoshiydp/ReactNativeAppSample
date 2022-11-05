9f3b36dc3b55da89147923120e982bffaad98e74
Yoshi Watanabe <yoshihisa.watanabe.info@gmail.com>
2022-11-05 15:18:03 +0900

    Add template component

---
+++ src/components/templates/MainScreen/index.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import { View, ScrollView } from 'react-native';
+
+// Components
+import HeaderTitle from '../../organisms/HeaderTitle';
+
+// Styles
+import styles from './MainScreen.scss';
+
+interface Props {
+  title: string;
+}
+
+const MainScreen = (props: Props) => {
+
+  useEffect(() => {
+  }, []);
+
+  return (
+    <View style={ styles.container }>
+      <HeaderTitle
+        title={ props.title }
+      />
+      <ScrollView>
+        {/* <Button title="RecordAudio" onPress={() => props.navigation.navigate('RecordAudio')} /> */}
+        {/* <View style={styles.block }></View> */}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default MainScreen;
