efb6c3d38d52286ad4d7d03363ba73fa1e0ad61a
Yoshi Watanabe <yoshihisa.watanabe.info@gmail.com>
2022-11-05 19:53:58 +0900

    Update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,7 @@
 import 'react-native-gesture-handler';
 import React, {type PropsWithChildren, useEffect} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-const Stack = createStackNavigator();
-
-// Screens
-import LoadingScreen from './screens/Loading';
-import LoginScreen from './screens/Login';
-import SignUpScreen from './screens/SignUp';
-
-// Components
-import MainTabBar from './components/organisms/MainTabBar';
+import StackNavigator from './navigators/StackNavigator';
 
 const App = () => {
 
@@ -28,22 +10,7 @@ const App = () => {
 
   return (
     <NavigationContainer>
-      <MainTabBar />
-      {/* <Stack.Navigator
-        screenOptions={{ headerShown: false }}>
-        <Stack.Screen
-          name="Loading"
-          component={ LoadingScreen }
-        />
-        <Stack.Screen
-          name="Login"
-          component={ LoginScreen }
-        />
-        <Stack.Screen
-          name="SignUp"
-          component={ SignUpScreen }
-        />
-      </Stack.Navigator> */}
+      <StackNavigator />
     </NavigationContainer>
   );
 };
