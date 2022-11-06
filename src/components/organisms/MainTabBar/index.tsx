a0d7ba524c956488f7aa6e52e9a6ec08d7f93284
Yoshi Watanabe <yoshihisa.watanabe.info@gmail.com>
2022-11-06 10:00:29 +0900

    Update MainTabBar

--- src/components/organisms/MainTabBar/index.tsx
+++ src/components/organisms/MainTabBar/index.tsx
@@ -10,6 +10,14 @@ import TrackListScreen from '../../../screens/TrackList';
 import RecordAudioScreen from '../../../screens/RecordAudio';
 import MyAccountScreen from '../../../screens/MyAccount';
 
+// Components
+import Icon from '../../atoms/Icon';
+
+// Constants
+import * as COLOR from '../../../constants/color';
+import * as SVGPATH from '../../../constants/svgPath';
+
+
 // Styles
 import styles from './MainTabBar.scss';
 
@@ -30,18 +38,78 @@ const MainNavBar = () => {
         <Tab.Screen
           name="MyProjects"
           component={ MyProjectsScreen }
+          options={{
+            tabBarLabel: '',
+            tabBarItemStyle: styles.tabBarLabel,
+            tabBarIcon: () => (
+              <Icon
+                width="8"
+                height="14.034"
+                viewBox="0 0 8 14.034"
+                gTransform="translate(-97.139 0)"
+                pathD={SVGPATH.ICON_ARROW_RIGHT}
+                pathTransform="translate(0 0)"
+                pathFill={COLOR.COLOR_GRAY_TYPE1}
+              />
+            ),
+          }}
         />
         <Tab.Screen
           name="TrackList"
           component={ TrackListScreen }
+          options={{
+            tabBarLabel: '',
+            tabBarItemStyle: styles.tabBarLabel,
+            tabBarIcon: () => (
+              <Icon
+                width="8"
+                height="14.034"
+                viewBox="0 0 8 14.034"
+                gTransform="translate(-97.139 0)"
+                pathD={SVGPATH.ICON_ARROW_RIGHT}
+                pathTransform="translate(0 0)"
+                pathFill={COLOR.COLOR_GRAY_TYPE1}
+              />
+            ),
+          }}
         />
         <Tab.Screen
           name="RecordAudio"
           component={ RecordAudioScreen }
+          options={{
+            tabBarLabel: '',
+            tabBarItemStyle: styles.tabBarLabel,
+            tabBarIcon: () => (
+              <Icon
+                width="8"
+                height="14.034"
+                viewBox="0 0 8 14.034"
+                gTransform="translate(-97.139 0)"
+                pathD={SVGPATH.ICON_ARROW_RIGHT}
+                pathTransform="translate(0 0)"
+                pathFill={COLOR.COLOR_GRAY_TYPE1}
+              />
+            ),
+          }}
         />
         <Tab.Screen
           name="MyAccount"
           component={ MyAccountScreen }
+          options={{
+            tabBarLabel: '',
+            tabBarItemStyle: styles.tabBarLabel,
+            tabBarIcon: () => (
+              <Icon
+                width="8"
+                height="14.034"
+                viewBox="0 0 8 14.034"
+                gTransform="translate(-97.139 0)"
+                pathD={SVGPATH.ICON_ARROW_RIGHT}
+                pathTransform="translate(0 0)"
+                pathFill={COLOR.COLOR_GRAY_TYPE1}
+              />
+            ),
+          }}
         />
       </Tab.Navigator>
     </View>
