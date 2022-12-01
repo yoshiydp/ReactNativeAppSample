ea344238d71d2546a0b5b07f4984344a1a4abaa1
Yoshi Watanabe <yoshihisa.watanabe.info@gmail.com>
2022-12-01 23:29:57 +0900

    Delete config for firebase

--- src/config/config.ts
+++
@@ -1,9 +0,0 @@
-export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBShdE1r8BsejhjilhlFbBTxG9gK9ZnLME",
-  authDomain: "lyrics-a7ae4.firebaseapp.com",
-  databaseURL: "https://lyrics-a7ae4.firebaseio.com",
-  projectId: "lyrics-a7ae4",
-  storageBucket: "lyrics-a7ae4.appspot.com",
-  messagingSenderId: "",
-  appId: "1:284037821861:ios:b8e654d3719d6334b6a44c"
-};
