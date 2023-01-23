9118a80224b561ab1c21f53ce5866bdf8539b02e
Yoshi Watanabe <yoshihisa.watanabe.info@gmail.com>
2023-01-23 17:17:11 +0900

    Development swipe gesture in MyProjectsItem

--- src/components/organisms/MyProjects/MyProjectsItem/index.tsx
+++ src/components/organisms/MyProjects/MyProjectsItem/index.tsx
@@ -1,8 +1,14 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { View, Text} from 'react-native';
+import { TouchableOpacity, View, Image, Text} from 'react-native';
+import { useDispatch } from 'react-redux';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+
+// Store
+import { useSelector } from 'store/index';
+import { showOverlay, inactiveHidden } from 'store/OverlaySlice';
 
 // Components
-import SocialIcon from 'components/molecules/SocialIcon';
+import DeleteButton from 'components/atoms/DeleteButton';
 
 // Interfaces
 import { svgType1, svgType2, svgType3, svgType4, svgType5 } from 'interfaces/svgInterface';
@@ -10,7 +16,7 @@ import { svgType1, svgType2, svgType3, svgType4, svgType5 } from 'interfaces/svg
 // Styles
 import styles from './MyProjectsItem.scss';
 
-interface Props {
+interface DataProps {
   projectTitle: string;
   lyric: string;
   trackTitle: string;
@@ -18,26 +24,78 @@ interface Props {
   artWork: string;
 }
 
+interface Props extends DataProps {
+  navigation: any;
+}
+
 const MyProjectsItem = (props: Props) => {
+  const swipeable = useRef<Swipeable>(null);
+  const dispatch = useDispatch();
+  const activeHiddenState = useSelector((state) => state.overlay.inactiveHidden);
 
   useEffect(() => {
-  }, []);
+    console.log('activeHiddenState: ' + activeHiddenState);
+  }, [activeHiddenState]);
+
+  const renderRightActions = () => {
+    return (
+      <DeleteButton
+        onPressEvent={ onPressDeleteProject }
+      />
+    );
+  };
+
+  const navigateEditProject = async () => {
+    await props.navigation.navigate('EditProject');
+  };
+
+  const onPressRightSwipeActions = () => {
+    swipeable.current?.openRight();
+  };
+
+  const onPressDeleteProject = () => {
+    dispatch(showOverlay());
+    dispatch(inactiveHidden());
+  };
 
   return (
-    <View style={ styles.container }>
-      <View style={ styles.artwork }></View>
-      <View style={ styles.textWrap }>
-        <Text style={ styles.title }>
-          { props.projectTitle }
-        </Text>
-        <Text style={ styles.text }>
-          { props.lyric }
-        </Text>
-        <Text style={ styles.text }>
-          { props.trackTitle } / { props.artistName }
-        </Text>
-      </View>
-    </View>
+    <Swipeable
+      ref={ swipeable }
+      renderRightActions={ renderRightActions }
+      rightThreshold={ 40 }
+    >
+      <TouchableOpacity
+        style={ styles.container }
+        onPress={ navigateEditProject }
+        activeOpacity={ 1 }>
+        <View style={ styles.artwork }>
+          <Image
+            style={ styles.image }
+            source={
+              props.artWork
+              ? props.artWork
+              : require('src/assets/images/common/no-artwork.jpg')
+            }
+          />
+        </View>
+        <View style={ styles.textWrap }>
+          <Text style={ styles.title }>
+            { props.projectTitle }
+          </Text>
+          <Text style={ styles.text }>
+            { props.lyric }
+          </Text>
+          <Text style={ styles.text }>
+            { props.trackTitle } / { props.artistName }
+          </Text>
+        </View>
+        <TouchableOpacity
+          style={ styles.ellipsisButton }
+          onPress={ onPressRightSwipeActions }
+          activeOpacity={ 1 }>
+        </TouchableOpacity>
+      </TouchableOpacity>
+    </Swipeable>
   );
 };
 
