838a3d612e2f4d3f45fe38633cc82ad50183acdf
Yoshi Watanabe <yoshihisa.watanabe.info@gmail.com>
2023-01-23 19:33:08 +0900

    Add svg interface

--- src/components/organisms/MyProjects/MyProjectsItem/index.tsx
+++ src/components/organisms/MyProjects/MyProjectsItem/index.tsx
@@ -9,6 +9,11 @@ import { showOverlay, inactiveHidden } from 'store/OverlaySlice';
 
 // Components
 import DeleteButton from 'components/atoms/DeleteButton';
+import Icon from 'components/atoms/Icon';
+
+// Constants
+import * as COLOR from 'constants/color';
+import * as SVGPATH from 'constants/svgPath';
 
 // Interfaces
 import { svgType1, svgType2, svgType3, svgType4, svgType5 } from 'interfaces/svgInterface';
@@ -93,6 +98,20 @@ const MyProjectsItem = (props: Props) => {
           style={ styles.ellipsisButton }
           onPress={ onPressRightSwipeActions }
           activeOpacity={ 1 }>
+          <Icon
+            svgType={ 6 }
+            width="5"
+            height="21"
+            viewBox="0 0 5 21"
+            parentGTransform="translate(-357 -118)"
+            childGTransform="translate(698 -206) rotate(90)"
+            coordinate="2.5"
+            radius="2.5"
+            pathTransform1="translate(324 336)"
+            pathTransform2="translate(332 336)"
+            pathTransform3="translate(340 336)"
+            pathFill={ COLOR.COLOR_GRAY_TYPE3 }
+          />
         </TouchableOpacity>
       </TouchableOpacity>
     </Swipeable>
