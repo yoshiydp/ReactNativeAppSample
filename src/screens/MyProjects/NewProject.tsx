44922c0518fcf4f4ee4a473d3e876c70408329fd
Yoshi Watanabe <yoshihisa.watanabe.info@gmail.com>
2023-01-29 16:05:54 +0900

    Update NewProject

--- src/screens/MyProjects/NewProject.tsx
+++ src/screens/MyProjects/NewProject.tsx
@@ -6,20 +6,64 @@ import LowerScreen from 'components/templates/LowerScreen';
 // Constants
 import * as TEXT from 'constants/text';
 
+// Validators
+import {
+  validateUserName,
+  validateEmail,
+  validatePassword,
+  validateNetworkRequestFailed,
+  validateTooManyRequests
+} from 'src/validators/SignUpValidator';
+
+// Styles
+import styles from './MyProjects.scss';
+
 interface Props {
   navigation: any;
 }
 
 const NewProject = (props: Props) => {
+  const [userName, setUserName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [errorUserName, setErrorUserName] = useState<string>('');
+  const [errorEmail, setErrorEmail] = useState<string>('');
 
   useEffect(() => {
   }, []);
+
+  const createProject = async () => {
+    console.log('createProject');
+  }
+
+  // テキストフォームリスト
+  const formControlItems = [
+    {
+      label: TEXT.LABEL_INPUT_PROJECT_TITLE,
+      placeholder: TEXT.PLACEHOLDER_PROJECT_TITLE,
+      onChangeText: setUserName,
+      value: userName,
+      required: true,
+      errorText: errorUserName
+    },
+    {
+      label: TEXT.LABEL_INPUT_TRACK_DATA,
+      placeholder: TEXT.PLACEHOLDER_NO_TRACK,
+      onChangeText: setEmail,
+      value: email,
+      required: true,
+      notes: TEXT.LABEL_NOTES_TRACK_DATA,
+      errorText: errorEmail
+    },
+  ];
 
   return (
     <>
       <LowerScreen
         title={ TEXT.TITLE_NEW_PROJECT }
         navigation={ props.navigation }
+        formControlItems={ formControlItems }
+        submitText={ TEXT.BUTTON_START }
+        onPressSubmitEvent={ createProject }
       />
     </>
   );
