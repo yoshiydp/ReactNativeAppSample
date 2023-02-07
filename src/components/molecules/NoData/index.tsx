751031e2d502c51bbce90964cfc18b9bfc3e8509
Yoshi Watanabe <yoshihisa.watanabe.info@gmail.com>
2023-02-07 20:30:59 +0900

    Add NoData component

---
+++ src/components/molecules/NoData/index.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import { View, Text } from 'react-native';
+
+// Components
+import ButtonSquare from 'components/atoms/ButtonSquare';
+
+// Styles
+import styles from './NoData.scss';
+
+interface Props {
+  text: string;
+  buttonText: string;
+  onPressEvent: () => void;
+}
+
+const NoData = (props: Props) => {
+
+  useEffect(() => {
+  }, []);
+
+  return (
+    <View style={ styles.container }>
+      <Text style={ styles.text }>
+        { props.text }
+      </Text>
+      <View style={ styles.buttonWrap }>
+        <ButtonSquare
+          text={ props.buttonText }
+          onPressEvent={ props.onPressEvent }
+        />
+      </View>
+    </View>
+  );
+};
+
+export default NoData;
