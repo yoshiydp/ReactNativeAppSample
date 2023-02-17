fac0fc0045a92877d6022e3d8f65636d09b41f68
Yoshi Watanabe <yoshihisa.watanabe.info@gmail.com>
2023-02-17 22:03:35 +0900

    Update components for TrackList screen

--- src/screens/TrackList/index.tsx
+++ src/screens/TrackList/index.tsx
@@ -1,4 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { firebaseAuth, db } from "src/config/firebase";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
+
+// Store
+import { useSelector } from "store/index";
+import { setTrackListItems, setTrackListFilterItems } from "store/TrackListItemsSlice";
 
 // Components
 import MainScreen from "components/templates/MainScreen";
@@ -11,9 +18,50 @@ interface Props {
 }
 
 const TrackList = (props: Props) => {
+  const dispatch = useDispatch();
+  const trackListItems = useSelector((state) => state.trackListItems.trackListItems);
+  const [searchValue, setSearchValue] = useState<string>("");
+  const { uid }: any = firebaseAuth.currentUser;
+  if (!uid) return;
+  const docRef = doc(db, "users", uid);
+
+  const getTrackListData = async () => {
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      dispatch(setTrackListItems(docSnap.data().trackListData));
+    } else {
+      console.log("No such document!");
+    }
+  };
+
+  useEffect(() => {
+    getTrackListData();
+    onSnapshot(docRef, (doc) => {
+      dispatch(setTrackListItems(doc.data()?.trackListData));
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log("searchValue: " + searchValue);
+    searchFilter();
+  }, [searchValue]);
+
+  const searchFilter = () => {
+    if (searchValue) {
+      dispatch(setTrackListFilterItems(searchValue));
+    } else {
+      getTrackListData();
+    }
+  };
+
   return (
     <>
-      <MainScreen title={TEXT.TITLE_TRACK_LIST} navigation={props.navigation} />
+      <MainScreen
+        title={TEXT.TITLE_TRACK_LIST}
+        navigation={props.navigation}
+        trackListDataItems={trackListItems}
+        setSearchValue={setSearchValue}
+      />
     </>
   );
 };
