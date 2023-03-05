1f8f232ee6df9c89112788be3813a7dda9d62192
Yoshi Watanabe <yoshihisa.watanabe.info@gmail.com>
2023-03-05 12:45:31 +0900

    Replace screen directories

--- src/screens/EditProject/index.tsx
+++ src/screens/EditProject/index.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Animated, View, ScrollView } from "react-native";
 
 // Components
 import LowerScreen from "components/templates/LowerScreen";
+
+// Styles
+import styles from "./MyProjects.scss";
 
 interface Props {
   navigation: any;
