240b14e63dbb56dabf88bee7a06ee7f81fe5eaf4
Yoshi Watanabe <yoshihisa.watanabe.info@gmail.com>
2023-04-21 19:33:18 +0900

    Update config files

--- metro.config.js
+++ metro.config.js
@@ -2,14 +2,14 @@ const { getDefaultConfig } = require("metro-config");
 
 module.exports = (async () => {
   const {
-    resolver: { sourceExts }
+    resolver: { sourceExts },
   } = await getDefaultConfig();
   return {
     transformer: {
-      babelTransformerPath: require.resolve("react-native-sass-transformer")
+      babelTransformerPath: require.resolve("react-native-sass-transformer"),
     },
     resolver: {
-      sourceExts: [...sourceExts, "scss", "sass"]
-    }
+      sourceExts: [...sourceExts, "scss", "sass"],
+    },
   };
 })();
