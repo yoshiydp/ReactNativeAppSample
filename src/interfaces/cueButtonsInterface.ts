97b968b52f0156756f92a410508249d50e01a4d2
Yoshi Watanabe <yoshihisa.watanabe.info@gmail.com>
2023-04-24 20:17:21 +0900

    Add CueButtons interface

---
+++ src/interfaces/cueButtonsInterface.ts
@@ -0,0 +1,20 @@
+export interface CueButtonsNameType {
+  cueA: string;
+  cueB: string;
+  cueC: string;
+  cueD: string;
+  cueE: string;
+}
+
+export interface OnPressEventType {
+  onPressActiveCue: (cueType: string, name: string) => void;
+  onLongPressEvent: (flag: boolean, name: string) => void;
+}
+
+export interface SetCueButtonsType {
+  cueA: [{ flag: boolean }, { name: string }, { position?: number }];
+  cueB: [{ flag: boolean }, { name: string }, { position?: number }];
+  cueC: [{ flag: boolean }, { name: string }, { position?: number }];
+  cueD: [{ flag: boolean }, { name: string }, { position?: number }];
+  cueE: [{ flag: boolean }, { name: string }, { position?: number }];
+}
