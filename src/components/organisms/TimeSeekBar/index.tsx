0d06bc7685b318d2944ec683c3bcf252a8c61713
Yoshi Watanabe <yoshihisa.watanabe.info@gmail.com>
2023-05-04 19:17:54 +0900

    Update TimeSeekBar component

--- src/components/organisms/TimeSeekBar/index.tsx
+++ src/components/organisms/TimeSeekBar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { View, Text } from "react-native";
 import { useProgress } from "react-native-track-player";
 import { Slider, Icon } from "@rneui/themed";
