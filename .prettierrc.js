a10da55885ca088b42b0166c5494ea0936e9b3a2
Yoshi Watanabe <yoshihisa.watanabe.info@gmail.com>
2023-05-05 23:20:50 +0900

    Update eslintrc and prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,9 @@
 module.exports = {
-  singleQuote: false,
-  trailingComma: "es5",
   printWidth: 100,
+  endOfLine: "lf",
+  singleQuote: false,
+  semi: true,
+  tabWidth: 2,
+  indentStyle: "space",
+  trailingComma: "es5",
 };
