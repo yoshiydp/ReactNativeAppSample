9110b9501f6d3def05bd32e7e64dc643fee8945a
Yoshi Watanabe <yoshihisa.watanabe.info@gmail.com>
2023-05-05 18:46:12 +0900

    Update stylelintrc

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
-  extends: ["stylelint-config-recess-order"],
+  plugins: ["stylelint-scss"],
+  syntax: "scss",
+  customSyntax: "postcss-scss",
   rules: {
+    "scss/dollar-variable-pattern": "^[a-z]+([a-z0-9-]+[a-z0-9]+)?$",
+    "scss/at-function-pattern": "^[a-z]+([a-z0-9-]+[a-z0-9]+)?$",
+    "scss/at-mixin-pattern": "^[a-z]+([a-z0-9-]+[a-z0-9]+)?$",
     "selector-pseudo-element-colon-notation": "double",
     "scss/selector-no-union-class-name": true,
   },
