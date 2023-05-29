1f6d6d0693461fb24e84c070ceb990255fa35e33
Yoshi Watanabe <yoshihisa.watanabe.info@gmail.com>
2023-05-29 20:18:05 +0900

    Add SettingsFormControls components for ModalProjectSettings

---
+++ src/components/molecules/SettingsTextField/index.tsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, TextInput } from "react-native";
+
+// Styles
+import styles from "./SettingsTextField.scss";
+
+// Interfaces
+import { TextFieldType } from "interfaces/formControlsInterface";
+
+// Constants
+import * as COLOR from "constants/color";
+
+interface Props extends TextFieldType {
+  errorText?: string;
+}
+
+const SettingsTextField = (props: Props) => {
+  const [value, valueHandler] = useState(props.value ? props.value : "");
+
+  useEffect(() => {
+    valueHandler(props.value ? props.value : "");
+  }, [props.value]);
+
+  const onChangeText = (event: any) => {
+    valueHandler(event);
+
+    if (props.onChangeText) {
+      props.onChangeText(event);
+      console.log(event);
+    }
+  };
+
+  return (
+    <View style={props.errorText ? styles["container--error"] : styles.container}>
+      <View style={styles["label-wrap"]}>
+        <Text style={styles.label}>{props.label}</Text>
+        {props.required && <Text style={styles.required}>*</Text>}
+        {props.notes && <Text style={styles.notes}>{props.notes}</Text>}
+      </View>
+      <TextInput
+        style={styles.input}
+        placeholder={props.placeholder}
+        placeholderTextColor={COLOR.COLOR_GRAY_TYPE3}
+        onChangeText={onChangeText}
+        value={props.value}
+        secureTextEntry={props.secureText}
+        autoCapitalize="none"
+        editable={props.editable}
+        selectTextOnFocus={props.selectTextOnFocus}
+      />
+    </View>
+  );
+};
+
+export default SettingsTextField;
---
+++ src/components/organisms/SettingsFormControls/index.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import { View } from "react-native";
+
+// Components
+import SettingsTextField from "components/molecules/SettingsTextField";
+import ErrorText from "components/atoms/ErrorText";
+
+// Interfaces
+import { FormControlsType } from "interfaces/formControlsInterface";
+
+// Styles
+import styles from "./SettingsFormControls.scss";
+
+interface Props {
+  formControlItems: Array<FormControlsType>;
+}
+
+const SettingsFormControls = (props: Props) => {
+  const [disabled, setDisabled] = useState<boolean>(false);
+
+  useEffect(() => {
+    props.formControlItems.map((item, index) => {
+      !item.value ? setDisabled(true) : setDisabled(false);
+    });
+  }, [props.formControlItems, disabled]);
+
+  return (
+    <>
+      {props.formControlItems.map((item, index) => (
+        <View style={styles.formControlItem} key={index}>
+          <SettingsTextField
+            label={item.label}
+            placeholder={item.placeholder}
+            onChangeText={item.onChangeText}
+            value={item.value}
+            secureText={item.secureText}
+            required={item.required}
+            notes={item.notes}
+            editable={item.editable}
+            selectTextOnFocus={item.selectTextOnFocus}
+            errorText={item.errorText}
+          />
+          {item.errorText && <ErrorText text={item.errorText} />}
+        </View>
+      ))}
+    </>
+  );
+};
+
+export default SettingsFormControls;
